21ed1c41078ffe2b21ce9c754eda1195ed3fa949
DearCCM <168969359+DearCCM@users.noreply.github.com>
2024-05-06 12:31:12 +0800

    Type_A_Acute_Aortic_Dissection_Surgery_AKI_APP.py

--- Type_A_Acute_Aortic_Dissection_Surgery_AKI_APP.py
+++ Type_A_Acute_Aortic_Dissection_Surgery_AKI_APP.py
@@ -5,7 +5,7 @@ import numpy as np
 # Load the AKI model
 aki_model = lgb.Booster(model_file='Type_A_Acute_Aortic_Dissection_Surgery_AKI_model.txt')
 
-hydragogue_mapping = {"0-20mg": 0, "20mg": 1, "＞200mg": 2}
+hydragogue = {"0-20mg": 0, "20mg": 1, "＞200mg": 2}
 ebrantil = {"with": 1, "without": 0}
 natriuretic_peptide = {"with": 1, "without": 0}
 
